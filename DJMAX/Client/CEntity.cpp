e15b6732dc7c0946973e2054830057d2e6dc51e7
superozing <amazingozing@gmail.com>
2023-10-05 18:17:48 +0900

    CEntity 클래스
    모든 객체, 레벨, 오브젝트, 레이어, 충돌체가 상속받아서 고유한 ID 값과 이름, Dead 여부를 관리할 수 있도록 하기 위해서 CEntity를 매니저와 CEngine을 제외한 모든 클래스가 상속받도록 구현

---
+++ DJMAX/Client/CEntity.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CEntity.h"
+
+UINT_PTR CEntity::g_NextID = 0;
+
+CEntity::CEntity()
+	:m_ID(g_NextID++)
+	//,m_bDead(false)
+{
+}
+
+// 복사 생성의 경우에도 id 값은 달라져야 한다
+CEntity::CEntity(const CEntity& _other) 
+	:m_ID(g_NextID++)
+	//,m_bDead(false)
+{
+}
+
+CEntity::~CEntity()
+{
+}
---
+++ DJMAX/Client/CEntity.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+class CEntity
+{
+	static UINT_PTR g_NextID;
+
+protected:
+	const UINT_PTR	m_ID;		// 고유 ID 값
+	wstring			m_strName;	// 객체 이름
+	//bool			m_bDead;	// 객체 삭제시 Dead 여부 체크
+
+public:
+	// 동일한 ID 값을 가진 특수한 객체 생성 시 사용할 것
+	virtual CEntity* Clone() { return nullptr; } 
+
+public:
+	CEntity();
+	CEntity(const CEntity& _other);
+	virtual ~CEntity();
+
+	//friend class CTaskMgr;
+	//friend class CObj;
+};
+
