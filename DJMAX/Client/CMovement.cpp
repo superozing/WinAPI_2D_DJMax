e77dfab56f107bedf90743054a9c5bc2546c6553
superozing <amazingozing@gmail.com>
2023-10-23 16:45:47 +0900

    Movement component 추가
    물리적 움직임을 사용하기 위해서 추가한 Movement component
    
    힘, 질량, 가속도를 방향 벡터로 계산해서 움직일 수 있게 해줌
    최소 속력과 최대 속력을 제한을 두어서 처음 움직일 때 너무 느린 속도로 움직이지 않도록, 가속도가 과도하게 붙어서 너무 빠르게 움직이지 않도록 해줌.
    
    중력을 사용할 수도 있고, 사용하지 않을 수 있음.
    중력은 (기본 값으로) 아래를 가리키는 방향 벡터에요.
    
    탑 뷰, 쿼터 뷰 형태의 게임은 중력이 필요 없기 때문에 중력을 사용하지 않아도 될 것이고,
    만약 기믹으로 중력이 뒤집히는... 이런 것도 구현이 가능하다.
    
    벽에 대한 충돌 처리는 오브젝트로부터 땅이나 벽 오브젝트를 따로 만들어서 충돌체에 의해서 setGround 상태를 조정해주어야 한다.

---
+++ DJMAX/Client/CMovement.cpp
@@ -0,0 +1,78 @@
+#include "pch.h"
+#include "CMovement.h"
+
+#include "CObj.h"
+
+CMovement::CMovement(CObj* _Owner)
+	:CComponent(_Owner)
+	, m_Mass(1.f)
+	, m_InitSpeed(0.f)
+	, m_MaxSpeed(0.f)
+	, m_FrictionScale(0.f)
+	, m_UseGravity(false)
+	, m_Ground(false)
+{
+}
+
+CMovement::~CMovement()
+{
+}
+
+void CMovement::finaltick(float _DT)
+{
+	// 힘 = 질량 * 가속도, 가속도 = 힘 / 질량 
+	// 방향 벡터
+	m_Accel = m_Force / m_Mass;
+
+	// 중력 적용
+	if (m_UseGravity && !m_Ground)
+	{
+		m_Accel += m_GravityForce;
+	}
+
+	// 정지 상태(와 가까운 상태)면서 가속도가 없을 경우
+	if (m_Velocity.Length() < 0.1f && (!m_Accel.IsZero()))
+	{
+		// 초기 속도를 맟춰줌
+		Vec2 vAccelDir = m_Accel;
+		vAccelDir.Normalize(); // 방향 벡터
+		m_Velocity = vAccelDir * m_InitSpeed;
+	}
+	else
+	{
+		m_Velocity += m_Accel * _DT;
+	}
+
+	// 최대 속도 제한
+	if (fabs(m_Velocity.x) > m_MaxSpeed)
+	{
+		m_Velocity.x = (m_Velocity.x / fabs(m_Velocity.x)) * m_MaxSpeed;
+	}
+
+	// 물체에 적용되고 있는 힘이 없으면 마찰력을 적용시킴
+	if (m_Force.IsZero() && m_Velocity.x != 0.f && m_Ground)
+	{
+		float fFriction = -m_Velocity.x;
+		fFriction /= fabs(fFriction);
+
+		fFriction *= m_FrictionScale;
+
+		float fFrictionAccel = (fFriction / m_Mass) * _DT;
+		if (fabs(m_Velocity.x) < fabs(fFrictionAccel))
+		{
+			m_Velocity = Vec2(0.f, m_Velocity.y);
+		}
+		else
+		{
+			m_Velocity.x += fFrictionAccel;
+		}
+	}
+	
+	// 힘 설정
+	Vec2 vObjPos = GetOwner()->GetPos();
+	vObjPos += m_Velocity * _DT;
+	GetOwner()->SetPos(vObjPos);
+
+	// 힘 리셋
+	m_Force = Vec2(0.f, 0.f);
+}
